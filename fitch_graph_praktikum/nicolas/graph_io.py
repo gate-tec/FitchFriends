0eaf29ae35b68c8df7090998cbad9b74f8366f73
Nicolas Handke <nicolas.handke@gmx.de>
2024-01-04 11:42:46 +0100

    Update typing

--- fitch_graph_praktikum/nicolas/graph_io.py
+++ fitch_graph_praktikum/nicolas/graph_io.py
@@ -2,9 +2,12 @@ import os.path
 from os.path import abspath, dirname
 from inspect import getsourcefile
 
-from typing import Dict, Union, List, Set
+from typing import TYPE_CHECKING, Literal, Union, overload
+from fitch_graph_praktikum.util.typing import RelationDictionary
 import pickle as pk
 
+if TYPE_CHECKING:
+    from typing import Any, Dict, Optional
 
 _options = [
     "D0.3_L0.3_H0.9", "D0.5_L0.5_H0.5", "D0.5_L0.5_H0.25", "D0.5_L0.5_H1.0",
@@ -13,7 +16,39 @@ _options = [
 _instances = list(range(100))
 _rel_path = os.path.join(dirname(abspath(getsourcefile(lambda: 0))), '../../graph-prak-GFH')
 
-def load_relations(x_leaves, x_options, x_instances) -> Dict[Union[int, str], List]:
+
+def load_relations(
+        x_leaves: Literal[10, 15, 20, 25, 30],
+        x_options: int,
+        x_instances: int
+) -> "RelationDictionary":
+    """
+    Load relations `E_0`, `E_1`, and `E_d` from specified xenologous graph.
+
+    Parameters
+    ----------
+    x_leaves: {10, 15, 20, 25, 30}
+        Number of leaves in the xenologous graph.
+    x_options: int
+        Specific configuration (must be between 0 and 6, inclusive).
+    x_instances: int
+        Specific instance of xenologous graph (must be between 0 and 99, inclusive).
+
+    Returns
+    -------
+    RelationDictionary
+        Dictionary of relations with keys `0`, `1`, and `'d'` respectively.
+
+    Raises
+    ------
+    ValueError
+        if `x_options` or `x_instances` is outside the given boundaries.
+    """
+    if not (0 <= x_options <= 6):
+        raise ValueError(f'x_options must be between 0 and 6, inclusive. Got {x_options} instead')
+    if not (0 <= x_instances <= 99):
+        raise ValueError(f'x_instances must be between 0 and 99, inclusive. Got {x_instances} instead')
+
     path = f"{_rel_path}/n{x_leaves}/{_options[x_options]}/{_options[x_options]}_n{x_leaves}_{_instances[x_instances]}"
     with open(f"{path}/biRelations.pkl", 'rb') as bi_file:
         bi_relations = pk.load(bi_file)
---
+++ fitch_graph_praktikum/util/typing.py
@@ -0,0 +1,7 @@
+from typing import TypedDict
+
+
+RelationDictionary = TypedDict(
+    'RelationDictionary',
+    {0: list, 1: list, 'd': list}
+)
