62a5b4167a213fdbfeb7d7f6426553b75ee7d4d5
Dertalex <kleinstadttauber@gmail.com>
2024-01-08 11:00:19 +0100

    added create_partial_tuples_cumulativeLoss

---
+++ fitch_graph_praktikum/alex/WP1/benchmarking.py
@@ -0,0 +1,10 @@
+import time
+# auf alle originalen Fitch-Graphen 2. und 2b generieren mit je 10-90 Loss (nicht kumulativ)
+
+def benchmark_algorithms(sampleID,relations):
+    algo1_start =
+    #do algo1 (012),(120),(210), track, start-end
+    #do algo2, track, start-end
+    #return relation/SampleID, graph1output, algo1-symmDiff, timestamps, graph2output,algo2Graph-symmDiff, timestamps
+    #Fehlerbewertung
+    return relations, sampleID relation/SampleID, graph1output, algo1-symmDiff, timestamps, graph2output,algo2Graph-symmDiff, timestamps
