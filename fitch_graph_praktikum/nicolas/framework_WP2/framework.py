6e462f6ae850934b22a07bc027bdb3ff27d91730
Nicolas Handke <nicolas.handke@gmx.de>
2024-01-09 11:51:54 +0100

    Update

--- fitch_graph_praktikum/nicolas/framework_WP2/framework.py
+++ fitch_graph_praktikum/nicolas/framework_WP2/framework.py
@@ -2,11 +2,22 @@ import random
 
 import networkx as nx
 import copy
+import numpy as np
 
 from typing import TYPE_CHECKING, Optional, Callable, Literal
 
 if TYPE_CHECKING:
     from typing import Any, Dict, Generator
+
+
+def log_sig(a, b):
+    x0 = min(a, b)
+    x1 = max(a, b)
+    if x0 < 0.0:
+        raise ValueError('function log_sig is only defined on positive values.')
+    if x0 <= 0.0000000000000001 or x1 <= 0.0000000000000001:
+        return 0.0
+    return 2 / (1 + np.exp(-np.log2(x0 / x1)))
 
 
 class Partition:
@@ -25,7 +36,7 @@ class Partition:
         # convert all node representations to frozensets
         relabeled_graph = nx.relabel_nodes(graph, {node: frozenset({node}) for node in graph.nodes})
         # get relations and weights from base graph
-        neighbors = {node: dict(relabeled_graph[node]) for node in relabeled_graph.nodes}
+        neighbors = {node: set(relabeled_graph[node].keys()) for node in relabeled_graph.nodes}
         # construct singleton partition
         return cls([[node] for node in relabeled_graph.nodes], [node for node in relabeled_graph.nodes], neighbors)
 
@@ -62,10 +73,26 @@ class Partition:
             yield Partition(new_partition_list, self.nodes, self.neighbors)
 
     def aggregate(self):
-        self.partition_list = [[frozenset({node for entry in community for node in entry})] for community in self.partition_list]
-        self.nodes = [community[0] for community in self.partition_list]
+        new_partition_list = [
+            [frozenset({node for entry in community for node in entry})] for community in self.partition_list
+        ]
+        self.nodes = [community[0] for community in new_partition_list]
         # recalculate neighbors (using average weight per aggregated edges)
-        # TODO
+        # TODO: only necessary for Leiden (possibly class extension)
+        new_neighbors = {node: set() for node in self.nodes}
+        for i, old_community in enumerate(self.partition_list):
+            new_node = new_partition_list[i][0]
+            for old_node in old_community:
+                for old_neighbor in self.neighbors[old_node]:
+                    if old_neighbor not in old_community:
+                        for j, other_old_community in enumerate(self.partition_list):
+                            if i != j and old_neighbor in other_old_community:
+                                new_neighbor = new_partition_list[j][0]
+                                new_neighbors[new_node].add(new_neighbor)
+                                new_neighbors[new_neighbor].add(new_node)
+
+        self.partition_list = new_partition_list
+        self.neighbors = new_neighbors
 
     def get_nodes(self):
         return self.nodes
@@ -73,12 +100,71 @@ class Partition:
     def to_flat_partition(self):
         return [[node for entry in community for node in entry] for community in self.partition_list]
 
+    def get_community(self, i):
+        return self.partition_list[i]
+
+    def get_community_for_base_node(self, base_node) -> int:
+        for i, community in enumerate(self.partition_list):
+            for entry in community:
+                if base_node in entry:
+                    return i
+        raise KeyError(f'Base node {base_node} not in partition.')
+
     def __len__(self):
         return len(self.partition_list)
 
 
-def average_weight_per_edge_cut(graph: nx.Graph, partition: Partition) -> "float":
-    return 1.0
+def average_weight_per_edge_cut(graph: nx.Graph, partition: Partition, val_lambda: float = 1) -> "float":
+    node_mapping = {node: None for node in graph.nodes}
+    community_data = {i: {'weight': 0.0, 'edges': 0, 'nodes': 0} for i in range(len(partition))}
+    external_edge_weights = 0.0
+
+    min_weight = None
+    max_weight = None
+
+    num_edges = 0
+    num_cut_edges = 0
+    for node1, node2, data in graph.edges(data=True):
+        num_edges += 1
+
+        if min_weight is None or data['weight'] < min_weight:
+            min_weight = data['weight']
+        if max_weight is None or data['weight'] > max_weight:
+            max_weight = data['weight']
+
+        if node_mapping[node1] is None:
+            community1 = partition.get_community_for_base_node(node1)
+            node_mapping[node1] = community1
+            community_data[community1]['nodes'] += 1
+        else:
+            community1 = node_mapping[node1]
+        if node_mapping[node2] is None:
+            community2 = partition.get_community_for_base_node(node2)
+            node_mapping[node2] = community2
+            community_data[community2]['nodes'] += 1
+        else:
+            community2 = node_mapping[node2]
+
+        if community1 != community2:
+            external_edge_weights += data['weight']
+            num_cut_edges += 1
+        else:
+            community_data[community1]['edges'] += 1
+            community_data[community1]['weight'] += data['weight']
+
+    # Internal term
+    internal_degrees = 0
+    for i, data in community_data.items():
+        # squared sum of internal degrees
+        internal_degrees += (2 * data['edges']) * (2 * data['edges'])
+    internal_term = 0.01 * internal_degrees / (2 * num_edges)  # rescale degree
+
+    # External term
+    average_weight_per_cut_edge = external_edge_weights / num_cut_edges if num_cut_edges > 0 else 0.0
+    external_term = log_sig(average_weight_per_cut_edge, max_weight)
+
+    # good value for lambda ~0.01
+    return external_term + val_lambda * internal_term
 
 
 #########################
@@ -143,3 +229,15 @@ if __name__ == '__main__':
     test_graph.add_edge(2, 0, weight=random.random())
     test_graph.add_edge(0, 3, weight=random.random())
     test_graph.add_edge(3, 4, weight=random.random())
+
+    part = Partition.singleton_partition_from_graph(test_graph)
+
+    part2 = next(part.get_partitions_for_node(part.get_nodes()[3]))
+
+    part2.aggregate()
+    for k, v in part2.neighbors.items():
+        print(f"{k}: {v}")
+
+
+    # test_part = partition_louvain(test_graph)
+    # print(test_part)
