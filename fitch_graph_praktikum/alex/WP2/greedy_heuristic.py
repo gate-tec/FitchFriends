781c5e914aed8cb17be464297256228a812a2aca
Dertalex <kleinstadttauber@gmail.com>
2024-01-11 11:15:53 +0100

    removed syntax error

--- fitch_graph_praktikum/alex/WP2/greedy_heuristic.py
+++ fitch_graph_praktikum/alex/WP2/greedy_heuristic.py
@@ -1,4 +1,5 @@
 from fitch_graph_praktikum.util.lib import partition_heuristic_scaffold
 
-def average_weight_of_edges_cut
-test = partition_heuristic_scaffold({},{},{},[],#average_weight_of_edges_cut)
+def average_weight_of_edges_cut():
+    #test = partition_heuristic_scaffold({},{},{},[])
+    pass
