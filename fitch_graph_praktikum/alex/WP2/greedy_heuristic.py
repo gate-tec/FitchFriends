c94b392af44361bec12732623d09f0dd0e295f36
Dertalex <kleinstadttauber@gmail.com>
2024-01-11 11:13:19 +0100

    updated bencharking functions

---
+++ fitch_graph_praktikum/alex/WP2/greedy_heuristic.py
@@ -0,0 +1,4 @@
+from fitch_graph_praktikum.util.lib import partition_heuristic_scaffold
+
+def average_weight_of_edges_cut
+test = partition_heuristic_scaffold({},{},{},[],#average_weight_of_edges_cut)
