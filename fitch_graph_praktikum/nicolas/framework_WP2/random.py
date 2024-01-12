ea9779439ac64aae711f5c00ddcbf8a9be52e674
Nicolas Handke <nicolas.handke@gmx.de>
2024-01-12 16:05:01 +0100

    Update benchmarking WP2

--- fitch_graph_praktikum/nicolas/framework_WP2/random.py
+++ fitch_graph_praktikum/nicolas/framework_WP2/random.py
@@ -9,11 +9,12 @@ import networkx as nx
 
 
 def partition_random(graph: nx.Graph) -> "list[list]":
-    partition = [[], []]
-    for node in graph.nodes:
-        if random.random() > 0.5:
-            partition[0].append(node)
+    graph_nodes = [x for x in graph.nodes]
+    partition = [[graph_nodes[0]], [graph_nodes[1]]]
+    for i in range(2, len(graph_nodes)):
+        if random.random() >= 0.5:
+            partition[0].append(graph_nodes[i])
         else:
-            partition[1].append(node)
+            partition[1].append(graph_nodes[i])
 
     return partition
