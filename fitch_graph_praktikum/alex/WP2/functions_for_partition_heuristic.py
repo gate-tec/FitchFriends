a6fd5517065742137eb47a49810020b1231889c4
Dertalex <kleinstadttauber@gmail.com>
2024-01-12 11:19:46 +0100

    benchmarking done for original fitch graphs (all relations)

--- fitch_graph_praktikum/alex/WP2/functions_for_partition_heuristic.py
+++ fitch_graph_praktikum/alex/WP2/functions_for_partition_heuristic.py
@@ -1,3 +1,5 @@
+import random
+
 import numpy
 
 from fitch_graph_praktikum.alex.WP2.full_weighted_relations import generate_full_weighted_relations
@@ -27,27 +29,36 @@ def sum_weight_scoring(left: [], right: [], relations: dict[tuple[int, int], flo
     return score
 
 
-
 def greedy_bipartition(graph: nx.Graph):
     nodes = list(graph.nodes)
     edges = {(n1, n2): v['weight'] for n1, n2, v in graph.edges(data=True)}
+    score_old = -1
+    score_new = 0
+    c = random.choice(nodes)
+    neighbors = graph.neighbors(c)
+    while score_new > score_old:
 
-
+        c = random.choice(nodes)
+        neighbors = graph.neighbors(c)
+        for neighbor in neighbors:
+        score = score + graph[c][neighbor]['weight']
+    while
     # test = partition_heuristic_scaffold({}, {}, {}, [], partition_function=0, scoring_function=0)
 
-    if __name__ == '__main__':
-        # print(numpy.random.normal)
-        # test_relations = {0: [(1, 2), (2, 1)], 1: [(0, 1), (1, 0)], 'd': [(0, 2)]}
-        # test_weights_sym = generate_full_weighted_relations(3, test_relations, numpy.random.normal, [3, 0.75],
-        #                                                     numpy.random.normal, [1, 0.5], symmetric=True)
-        # test_weights_asym = generate_weighted_relations(test_relations, numpy.random.normal, [3, 0.75], symmetric=False)
-        # print(test_weights_sym)
-        # for k, v in test_weights_sym.items():
-        #     print(f"{k}: {v}")
-        #
-        # test = partition_heuristic_scaffold({}, {}, {}, [], partition_function=0, scoring_function=0)
 
-        edges = [(0, 1), (2, 0), (1, 2), (0, 3), (1, 3), (2, 3)]
-        G = nx.Graph()
-        G.add_edges_from(edges)
-        print(G.edges)
+if __name__ == '__main__':
+    # print(numpy.random.normal)
+    # test_relations = {0: [(1, 2), (2, 1)], 1: [(0, 1), (1, 0)], 'd': [(0, 2)]}
+    # test_weights_sym = generate_full_weighted_relations(3, test_relations, numpy.random.normal, [3, 0.75],
+    #                                                     numpy.random.normal, [1, 0.5], symmetric=True)
+    # test_weights_asym = generate_weighted_relations(test_relations, numpy.random.normal, [3, 0.75], symmetric=False)
+    # print(test_weights_sym)
+    # for k, v in test_weights_sym.items():
+    #     print(f"{k}: {v}")
+    #
+    # test = partition_heuristic_scaffold({}, {}, {}, [], partition_function=0, scoring_function=0)
+
+    edges = [(0, 1, 2.0), (2, 0, 3.0), (1, 2, 0.5), (0, 3, 0.9), (1, 3, 1.5), (2, 3, 2.0)]
+    G = nx.Graph()
+    G.add_weighted_edges_from(edges)
+    print(G[0][1])
