2ed657a58fc32842634689d87370864c439d48e8
Nicolas Handke <nicolas.handke@gmx.de>
2024-01-15 12:05:11 +0100

    Update

--- fitch_graph_praktikum/state_2024_01_12.py
+++ fitch_graph_praktikum/state_2024_01_12.py
@@ -12,5 +12,5 @@ for i, (mu_TP, mu_FP) in enumerate(tuples):
     print(f"#####\nRunning benchmark {i + 1}/{len(tuples)}\n#####")
     benchmark_df, weight_df = benchmark_all_stored_graphs(mu_TP=mu_TP, mu_FP=mu_FP, safety_steps=500)
 
-    save_dataframe(f'bm2_{mu_TP}_{mu_FP}_results', benchmark_df)
-    save_dataframe(f'bm2_{mu_TP}_{mu_FP}_weights', benchmark_df)
+    save_dataframe(f'bm2_{mu_TP}_{mu_FP}_results.tsv', benchmark_df)
+    save_dataframe(f'bm2_{mu_TP}_{mu_FP}_weights.tsv', benchmark_df)
